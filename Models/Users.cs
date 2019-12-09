bcca2c78f9011768cada607a69fdcdb8bedbfeff
rgoonie <rajeev.goonie98@gmail.com>
2019-12-08 23:14:01 -0600

    actually saved the code this time

---
+++ Models/Users.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+
+    public class User
+    {
+
+        public int userID { get; set; }
+        public String displayName { get; set; }
+
+        public DateTime graduationDate { get; set; }
+
+    }
+
+}
