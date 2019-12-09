c25f3056d3688dcd43d0c4c37c7cd10de8d89e89
rgoonie <rajeev.goonie98@gmail.com>
2019-12-08 23:22:01 -0600

    Created Basic Event Model

---
+++ Models/Event.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+
+    public class Event
+    {
+
+        public int eventID { get; set; }
+        public String name { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
+
+    }
+
+}
