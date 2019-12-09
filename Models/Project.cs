279b610fa6b41d95bc6e12465932d1caec5b9ff8
rgoonie <rajeev.goonie98@gmail.com>
2019-12-08 23:18:49 -0600

    Created Basic Project Model

---
+++ Models/Project.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+
+    public class Project
+    {
+
+        public int projectID { get; set; }
+        public String name { get; set; }
+
+        public String description { get; set; }
+
+        public int projectOwner { get; set; }
+
+    }
+
+}
