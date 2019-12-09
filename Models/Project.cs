8d9c471f15277628c3cea71d4be7be4c5e9a63a8
rgoonie <43188765+rgoonie@users.noreply.github.com>
2019-12-09 00:39:40 -0600

    created basic models for users, projects, and events (#6)
    * Created Basic User Model
    
    * actually saved the code this time
    
    * Created Basic Project Model
    
    * Created Basic Event Model
    
    * added current model for user
    
    * added current model for projects
    
    * added current model for events
    
    * update all id's to correct name and type
    
    * update all id's to correct name and type
    
    * added missing brace

---
+++ Models/Project.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+
+    public class Project
+    {
+
+        public String id { get; set; }
+        public String name { get; set; }
+
+        public String description { get; set; }
+
+        public int projectOwner { get; set; }
+
+    }
+}
