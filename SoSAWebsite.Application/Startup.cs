275cb6180182e8e08be7ae7b14e20ffce7136a54
Miggy Reyes <ynigoreyes@gmail.com>
2019-12-17 13:29:25 -0600

    adding beginning of UserService to help out rajeev

--- SoSAWebsite.Application/Startup.cs
+++ SoSAWebsite.Application/Startup.cs
@@ -33,6 +33,8 @@ namespace SoSAWebsite.Application
                 return new ContainerFactory(Configuration.GetConnectionString("CosmosDb"));
             });
 
+            services.AddSingleton<UsersService>();
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
 
