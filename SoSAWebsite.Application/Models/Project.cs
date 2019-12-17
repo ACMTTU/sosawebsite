9448bbbed4ac8d2280098bd2c50b06f6d8381f30
Miggy Reyes <ynigoreyes@gmail.com>
2019-12-17 15:28:04 -0600

    adding scaffoldig and some views for the Admin Portal

--- SoSAWebsite.Application/Models/Project.cs
+++ SoSAWebsite.Application/Models/Project.cs
@@ -11,7 +11,7 @@ namespace Models
         public String name { get; set; }
         public String extendedDescription { get; set; }
         public String description { get; set; }
-        public int projectOwner { get; set; }
+        public String projectOwner { get; set; }
         public Boolean isNewProject { get; set; }
         public String imageSource { get; set; }
     }
