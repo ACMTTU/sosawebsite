6a21da07283a3135a7f2b2021ea62d35a1d34f13
rgoonie <rajeev.goonie98@gmail.com>
2019-12-31 00:18:31 -0600

    Created user service file based off FooService.cs

---
+++ SoSAWebsite.Application/Data/UserService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using Models
+using SoSAWebsite.Application.Data.ContainerManager;
+using Microsoft.Azure.Cosmos;
+
+namespace SoSAWebsite.Application.Data
+{
+    public class UserService
+    {
+
+        private Container container;
+
+        /// <summary>
+        /// Example for connecting to the database
+        /// </summary>
+        /// <param name="containerManagerFactory"></param>
+        public UserService(ContainerManagerFactory containerManagerFactory)
+        {
+            var containerManager = containerManagerFactory.GetContainerManager();
+            container = containerManager.GetContainer(DatabaseContainers.Users);
+        }
+    }
+}
